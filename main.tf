c5a549a9caef5073c011101103eddd5e1809883c
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-06-09 02:10:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "aws_internet_gateway" "main_igw" {
 #IGW Creation - SHS
 
 resource "aws_internet_gateway" "main_igw_shr" {
-  depends_on = [aws_ec2_transit_gateway.main_tgw,aws_internet_gateway.main_igw]
+  depends_on = [aws_ec2_transit_gateway.fw_tgw,aws_internet_gateway.main_igw]
   vpc_id = aws_vpc.shr_vpc.id
   tags = {
     Name = join("", [var.coid, "-IGW"])
@@ -155,12 +155,12 @@ resource "aws_route_table" "public_rt_shr" {
 #Private_rt_fw
 
 resource "aws_route_table" "private_rt" {
-  depends_on = [aws_internet_gateway.main_igw,aws_ec2_transit_gateway.main_tgw,aws_vpn_connection.Miami]
+  depends_on = [aws_internet_gateway.main_igw,aws_ec2_transit_gateway.fw_tgw,aws_vpn_connection.Miami]
   vpc_id = aws_vpc.fw_vpc.id
   
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_ec2_transit_gateway.main_tgw.id
+    gateway_id = aws_ec2_transit_gateway.fw_tgw.id
   }
   
   
@@ -172,12 +172,12 @@ resource "aws_route_table" "private_rt" {
 #Private_rt_shr
 
 resource "aws_route_table" "private_rt_shr" {
-  depends_on = [aws_internet_gateway.main_igw,aws_ec2_transit_gateway.main_tgw,aws_vpn_connection.Miami]
+  depends_on = [aws_internet_gateway.main_igw,aws_ec2_transit_gateway.fw_tgw,aws_vpn_connection.Miami]
   vpc_id = aws_vpc.shr_vpc.id
   
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_ec2_transit_gateway.main_tgw.id
+    gateway_id = aws_ec2_transit_gateway.fw_tgw.id
   }
   
   
@@ -368,25 +368,25 @@ resource "aws_vpn_connection" "Miami" {
 }
 
 data "aws_ec2_transit_gateway_vpn_attachment" "oak_attach" {
-  transit_gateway_id = aws_ec2_transit_gateway.main_tgw.id
+  transit_gateway_id = aws_ec2_transit_gateway.fw_tgw.id
   vpn_connection_id  = aws_vpn_connection.Oakbrook.id
 }
 
 data "aws_ec2_transit_gateway_vpn_attachment" "miami_attach" {
-  transit_gateway_id = aws_ec2_transit_gateway.main_tgw.id
+  transit_gateway_id = aws_ec2_transit_gateway.fw_tgw.id
   vpn_connection_id  = aws_vpn_connection.Miami.id
 }
 
 resource "aws_ec2_transit_gateway_route" "oak_vpn" {
   destination_cidr_block         = "10.159.94.0/23"
   transit_gateway_attachment_id  = data.aws_ec2_transit_gateway_vpn_attachment.oak_attach.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway.main_tgw.association_default_route_table_id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.fw_tgw.association_default_route_table_id
   blackhole                      = false
 }
 
 resource "aws_ec2_transit_gateway_route" "mia_vpn" {
   destination_cidr_block         = "10.189.0.0/23"
   transit_gateway_attachment_id  = data.aws_ec2_transit_gateway_vpn_attachment.miami_attach.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway.main_tgw.association_default_route_table_id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.fw_tgw.association_default_route_table_id
   blackhole                      = false
 }
